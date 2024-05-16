05dec47e5537485cadad1b0d9627cf2ca82531e5
raynerbeta <33467773+raynerbeta@users.noreply.github.com>
2024-05-16 10:38:36 -0400

    Complete login and register

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
 const cors = require('cors');
-const AuthenticationError = require('./errors/AuthenticationError')
+const DatabaseError = require('./errors/DatabaseError');
+const AuthenticationError = require('./errors/AuthenticationError');
+const ValidationError = require('./errors/ValidationError');
 
 // Defining constants from environment
 const APP_PORT = process.env.APP_PORT || 3000;
 const MONGO_HOST = process.env.MONGO_HOST || "localhost";
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const MONGO_USER = process.env.MONGO_USER || "root";
-const MONGO_PASS = process.env.MONGO_PASS || "OTgzNy1yYXluZXJi";
+const MONGO_PASS = process.env.MONGO_PASS || "NTE2My1yYXluZXJi";
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "restful_backend";
 const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}`;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "yourSecretKey";
@@ -29,16 +31,48 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// POST endpoint for user login
-app.post('/api/login', async (req, res, next) => {
-    const { usernameOrEmail, password } = req.body;
+app.post('/auth/register', async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || typeof (email) !== 'string') {
+        next(new ValidationError("Email and password required for registering"));
+    }
     const user = await Users.findOne({
-        $or: [{
-            username: usernameOrEmail
-        },
-        {
-            email: usernameOrEmail
-        }]
+        email
+    });
+    if (user) {
+        next(new ValidationError("Email already registered"));
+        return;
+    }
+    let hashedPwd = bcrypt.hashSync(password, JWT_SALT_ROUNDS);
+    let newUser = new Users({
+        email,
+        password: hashedPwd
+    });
+    newUser = await newUser.save()
+        .catch(err => {
+            console.log(err.stack);
+            next(new DatabaseError("Error while registering user"));
+        });
+    const payload = {
+        email: newUser.email
+    };
+    const token = jwt.sign(payload, JWT_SECRET_KEY, {
+        expiresIn: '1h'
+    })
+    res.status(200).send({
+        token,
+        message: "Successful registration"
+    });
+});
+
+// POST endpoint for user login
+app.post('/auth/login', async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || typeof (email) !== 'string') {
+        next(new ValidationError("Email and password are required for loging"));
+    }
+    const user = await Users.findOne({
+        email
     });
     if (!user) {
         next(new AuthenticationError("User not found"));
@@ -86,8 +120,8 @@ function authenticateToken(req, res, next) {
     })
 }
 
-app.get('/api/secure', authenticateToken, (req, res)=>{
-    res.send({message: "OK"})
+app.get('/api/secure', authenticateToken, (req, res) => {
+    res.send({ message: "OK" })
 })
 
 // Global error handler
@@ -99,7 +133,15 @@ app.use((err, req, res, next) => {
         statusCode: err.statusCode,
         message: err.message
     })
-})
+});
+/*
+app.all('*', (req, res) => {
+    res.status(404).json({
+        statusCode: 404,
+        message: `You requested a non-existent resource: ${req.originalUrl}`
+    });
+});
+*/
 
 // Starting the server and listening on the specified port
 app.listen(APP_PORT, () => {
